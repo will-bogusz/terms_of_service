a2a1b18718ee5aae9133c63ab58d13906e6881c4
Will Bogusz <wbogusz24@gmail.com>
2024-04-17 15:44:47 -0400

    added exclusion for certain users

--- lib/Listener/UserLoggedOutListener.php
+++ lib/Listener/UserLoggedOutListener.php
@@ -7,16 +7,50 @@ namespace OCA\TermsOfService\Listener;
 use OCA\TermsOfService\Db\Mapper\SignatoryMapper;
 use OCP\EventDispatcher\Event;
 use OCP\EventDispatcher\IEventListener;
+use OCP\IConfig;
+use OCP\IGroupManager;
+use OCP\ISession;
+use OCP\IUser;
 use OCP\User\Events\UserLoggedOutEvent;
 
 class UserLoggedOutListener implements IEventListener {
 
-	/** @var SignatoryMapper */
-	private $signatoryMapper;
+	/** @var ISession */
+    private $session;
 
-	public function __construct(SignatoryMapper $signatoryMapper) {
-		$this->signatoryMapper = $signatoryMapper;
-	}
+    /** @var IConfig */
+    private $config;
+
+    /** @var IGroupManager */
+    private $groupManager;
+
+    public function __construct(ISession $session, IConfig $config, IGroupManager $groupManager) {
+        $this->session = $session;
+        $this->config = $config;
+        $this->groupManager = $groupManager;
+    }
+
+    private function isExcludedUser(IUser $user): bool {
+        // check if the user belongs to a specific group
+        $excludedGroups = ['zaza', 'management'];
+        $userGroups = $this->groupManager->getUserGroupIds($user);
+        foreach ($excludedGroups as $groupName) {
+            if (in_array($groupName, $userGroups)) {
+                return true;
+            }
+        }
+
+        // check if the user's email ends with a specific domain
+        $excludedDomains = ['@foxguardsolutions.com'];
+        $userEmail = $user->getSystemEMailAddress();
+        foreach ($excludedDomains as $domain) {
+            if (str_ends_with($userEmail, $domain)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
 	public function handle(Event $event): void {
 		if (!($event instanceof UserLoggedOutEvent)) {
@@ -24,6 +58,13 @@ class UserLoggedOutListener implements IEventListener {
 			return;
 		}
 
-		$this->signatoryMapper->deleteSignatoriesByUser($event->getUser());
+        $user = $event->getUser();
+
+        // check if the user belongs to a specific group or has a specific email domain
+        if ($this->isExcludedUser($user)) {
+            return;
+        }
+
+		$this->signatoryMapper->deleteSignatoriesByUser($user);
 	}
 }
