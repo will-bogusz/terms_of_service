2088501860b97a11925996f38eb32852600536df
Will Bogusz <wbogusz24@gmail.com>
2024-04-17 16:50:17 -0400

    added untested cron job to force log outs after set time period

---
+++ lib/Cron/LogoutStaleUsers.php
@@ -0,0 +1,39 @@
+// lib/Cron/LogoutStaleUsers.php
+
+<?php
+
+namespace OCA\TermsOfService\Cron;
+
+use OC\BackgroundJob\TimedJob;
+use OCP\IUserManager;
+use OCP\IUserSession;
+
+class LogoutStaleUsers extends TimedJob {
+    /** @var IUserManager */
+    private $userManager;
+
+    /** @var IUserSession */
+    private $userSession;
+
+    public function __construct(IUserManager $userManager, IUserSession $userSession) {
+        $this->userManager = $userManager;
+        $this->userSession = $userSession;
+
+        // run the job every 30 seconds for testing purposes
+        $this->setInterval(30);
+    }
+
+    protected function run($argument) {
+        // return ALL users
+        $users = $this->userManager->search('');
+        $currentTime = time();
+        $thresholdTime = $currentTime - (1 * 60); // log the user out if they have been logged in for longer than a minute
+
+        foreach ($users as $user) {
+            $lastLogin = $user->getLastLogin();
+            if ($lastLogin !== null && $lastLogin < $thresholdTime) {
+                $this->userSession->logoutUser($user);
+            }
+        }
+    }
+}
